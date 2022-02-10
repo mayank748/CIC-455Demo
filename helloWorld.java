dbd119cae3a7f5e34ba955ce4b9e8fc4892770b1
Mayank <smayank748@gmail.com>
2022-02-10 09:16:24 +0530

    [Mayank]created a local repository

---
+++ helloWorld.java
@@ -0,0 +1,12 @@
+public class helloWorld{
+	public static void main(String[] args){
+		while(true){
+		System.out.println("Hello world");
+		try{
+			Thread.sleep(30000);
+		}
+		catch(Exception e){
+		}
+		}
+	}
+}
